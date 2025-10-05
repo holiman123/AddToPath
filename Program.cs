c27d73271010c862018674cbf93e5a8b3ad7aeef
holiman123 <luchikholiday@gmail.com>
2025-10-05 20:06:56 +0300

    Many fixes.

--- Program.cs
+++ Program.cs
@@ -6,52 +6,129 @@ internal class Program
 
     static void Main(string[] args)
     {
-        string envPath = Environment.CurrentDirectory;
-        Console.Write($"Adding new PATH: {envPath}... ");
-
-        string PathString = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User);
-        List<string> PathVars = PathString.Split(";").ToList();
-        int countBefore = PathVars.Count;
-        PathVars = PathVars[0..^1];
-        PathVars = PathVars.Distinct().ToList();
-        PathVars.Add(envPath);
-        string newPath = String.Join(";", PathVars);
-        newPath += ";";
-        Console.WriteLine(newPath);
-
-        var coord = Console.GetCursorPosition();
-        CancellationTokenSource cts = new CancellationTokenSource();
-        Task.Run(async () => 
-        { 
-            while(!cts.IsCancellationRequested)
-            {
-                Console.SetCursorPosition(coord.Left, coord.Top);
-                Console.Write("/");
-                await Task.Delay(delay);
-
-                Console.SetCursorPosition(coord.Left, coord.Top);
-                Console.Write("─");
-                await Task.Delay(delay);
-
-                Console.SetCursorPosition(coord.Left, coord.Top);
-                Console.Write("\\");
-                await Task.Delay(delay);
-
-                Console.SetCursorPosition(coord.Left, coord.Top);
-                Console.Write("|");
-                await Task.Delay(delay);
-            }
-        });
-        Environment.SetEnvironmentVariable("Path", newPath, EnvironmentVariableTarget.User);
-
-        cts.Cancel();
-
-        PathString = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User);
-        PathVars = PathString.Split(";").ToList();
-
-        if (countBefore == PathVars.Count)
-            Console.WriteLine("\nDid not added!");
+        if (args.Length == 1 && args[0] == "-r")
+        {
+            RemoveFromPath();
+        }
+        else if (args.Length == 1 && args[0] == "-c")
+        {
+            List<string> path = GetPath();
+            ShowPath(path);
+            Console.WriteLine();
+        }
+        else if (args.Length == 1 && (args[0] == "-h" || args[0] == "?" || args[0] == "--help"))
+        {
+            Console.Write(
+                "Use '-r' to remove existing PATH variable;\n" +
+                "Use '-c' to print current PATH;\n" +
+                "Use '-h' to print this help message.");
+        }
         else
-            Console.WriteLine("\nSuccess");
+        {
+            AddToPath();
+        }
+    }
+
+    private static void AddToPath()
+    {
+        string envPath = Environment.CurrentDirectory;
+        Console.WriteLine($"Adding new PATH: {envPath}... \n");
+
+        List<string> pathVars = GetPath();
+
+        if (pathVars.Contains(envPath))
+        {
+            ShowPath(pathVars);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nThis path already exist.");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!\n");
+            Console.ResetColor();
+        }
+        else
+        {
+            pathVars.Add(envPath);
+            string newPath = CreatePath(pathVars);
+
+            Environment.SetEnvironmentVariable("Path", newPath, EnvironmentVariableTarget.User);
+
+            pathVars = GetPath();
+            ShowPath(pathVars);
+
+            if (pathVars.Contains(envPath))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nSuccess!\n");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nFailer!\n");
+                Console.ResetColor();
+            }
+        }
+    }
+
+    private static void RemoveFromPath()
+    {
+        string envPath = Environment.CurrentDirectory;
+        Console.WriteLine($"Removing from PATH: {envPath}... \n");
+
+        List<string> pathVars = GetPath();
+
+        if (pathVars.Contains(envPath))
+        {
+            pathVars.Remove(envPath);
+            string newPath = CreatePath(pathVars);
+
+            Environment.SetEnvironmentVariable("Path", newPath, EnvironmentVariableTarget.User);
+
+            pathVars = GetPath();
+            ShowPath(pathVars);
+
+            if (!pathVars.Contains(envPath))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nSuccess!\n");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nFailer!\n");
+                Console.ResetColor();
+            }
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nThis path is not presented.");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!\n");
+            Console.ResetColor();
+        }
+    }
+
+    private static List<string> GetPath()
+    {
+        string PathString = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User);
+        List<string> path = PathString.Split(";").ToList();
+        path.RemoveAll((s) => s == "");
+        return path;
+    }
+
+    private static string CreatePath(List<string> path)
+    {
+        return String.Join(";", path) + ";";
+    }
+
+    private static void ShowPath(List<string> pathVars)
+    {
+        foreach (string var in pathVars)
+        {
+            Console.WriteLine(var + ";");
+        }
     }
 }
